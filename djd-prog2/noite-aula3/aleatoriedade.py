9503f19bd47e36fb2dbd5f5e9709c6c924dd7f78
antoniorcn <antoniorcn@hotmail.com>
2020-03-07 12:33:50 -0300

    Aula 4 noite PROG2 parte 3

---
+++ djd-prog2/noite-aula3/aleatoriedade.py
@@ -0,0 +1,7 @@
+# import random
+# a = random.random()
+# print(a)
+
+from random import random
+a = random()
+print(a)
---
+++ djd-prog2/noite-aula3/teste_seed.py
@@ -0,0 +1,9 @@
+from random import random, randint, randrange, seed
+seed(500)
+x1 = randint(0, 1000)
+x2 = randint(0, 1000)
+x3 = randint(0, 1000)
+x4 = randint(0, 1000)
+x5 = randint(0, 1000)
+
+print(x1, x2, x3, x4, x5)
