1efa5c995161603e89f649874a609183fb970280
antoniorcn <antoniorcn@hotmail.com>
2020-05-07 23:01:53 -0300

    .

--- ads-poo/noite-aula6/src/edu/curso/Layouts.java
+++ ads-poo/noite-aula6/src/edu/curso/Layouts.java
@@ -5,8 +5,12 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.TilePane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Layouts extends Application {
@@ -24,7 +28,7 @@ public class Layouts extends Application {
 		bp.setLeft(le);
 		bp.setTop(lc);
 		// bp.setBottom(lb);
-		bp.setCenter(lcentro);
+		// bp.setCenter(lcentro);
 		
 		BorderPane.setAlignment(ld, Pos.CENTER_RIGHT);
 		BorderPane.setAlignment(le, Pos.CENTER_LEFT);
@@ -36,10 +40,30 @@ public class Layouts extends Application {
 		flow.getChildren().add(new Button("Botão 2"));
 		flow.setMaxWidth(200);
 		
+		TilePane grid = new TilePane();
+		grid.setPrefColumns(2);
+		Label lblNome = new Label("Nome: ");
+		TextField txtNome = new TextField();
+		
+		Label lblTel = new Label("Telefone: ");
+		TextField txtTel = new TextField();
+		txtTel.setMinWidth(200);
+		
+//		grid.add(lblNome, 0, 0);
+//		grid.add(txtNome, 1, 0);
+//		grid.add(lblTel, 0, 1);
+//		grid.add(txtTel, 1, 1);
+		grid.getChildren().addAll(lblNome, txtNome,lblTel, txtTel);
+		
+		bp.setCenter(grid);
+		
 		bp.setBottom(flow);
 		
 		BorderPane.setAlignment(flow, Pos.BOTTOM_CENTER);
 		
+		Text txt = new Text("Um carneirinho foi passear, sobre as montanhas e desceu ao mar.");
+		txt.setWrappingWidth(100);
+		bp.setLeft(txt);
 		
 		stage.setScene(scn);
 		stage.setTitle("Teste de Layouts");
