c84e75c5fa555ef4dbfdb8e963e784314a9a4475
antoniorcn <antoniorcn@hotmail.com>
2020-05-14 16:44:37 -0300

    .

--- ads-poo/tarde-aula7/src/edu/curso/SistemaSolar.java
+++ ads-poo/tarde-aula7/src/edu/curso/SistemaSolar.java
@@ -2,6 +2,7 @@ package edu.curso;
 
 import java.io.InputStream;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -9,9 +10,10 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
 public class SistemaSolar extends Application {
-	
+	class Ponto { 
+		double angulo = 0;
+	}
 	public void start(Stage stage) { 
 		Group grp = new Group();
 		Scene scn = new Scene(grp);
@@ -22,15 +24,26 @@ public class SistemaSolar extends Application {
 		Image imgSun = new Image(getClass().getResourceAsStream("/images/sun.png"));
 		Image imgEarth = new Image(getClass().getResourceAsStream("/images/earth.png"));
 		Image imgMars = new Image(getClass().getResourceAsStream("/images/mars.png"));
-		
-		
 		Canvas canvas = new Canvas(imgSpace.getWidth(), imgSpace.getHeight());
-		grp.getChildren().add(canvas);
-		
+		grp.getChildren().add(canvas);	
 		GraphicsContext ctx = canvas.getGraphicsContext2D();
-		ctx.drawImage(imgSpace, 0, 0);
-		ctx.drawImage(imgSun, imgSpace.getWidth() / 2, imgSpace.getHeight() / 2);
-		
+		final double solX = imgSpace.getWidth() / 2;
+		final double solY = imgSpace.getHeight() / 2;
+		final Ponto p = new Ponto();
+		new AnimationTimer() {
+			@Override
+			public void handle(long now) {
+				double terraX = 0;
+				double terraY = 0;
+				double terraDistancia = 100;
+				ctx.drawImage(imgSpace, 0, 0);
+				ctx.drawImage(imgSun, solX, solY);
+				terraX = (solX + imgSun.getWidth() / 2) + (terraDistancia * Math.cos(p.angulo));
+				terraY = (solY + imgSun.getHeight() / 2) + (terraDistancia * Math.sin(p.angulo));
+				ctx.drawImage(imgEarth, terraX, terraY);
+				p.angulo += 0.01;
+			} 
+		}.start();
 		stage.setTitle("Sistema Solar");
 		stage.show();		
 	}
@@ -40,3 +53,4 @@ public class SistemaSolar extends Application {
 	}
 
 }
+
