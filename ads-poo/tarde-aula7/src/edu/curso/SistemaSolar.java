51ec36c8bedcf5d5c2322124d7a11ebaa10c5193
antoniorcn <antoniorcn@hotmail.com>
2020-05-14 17:43:59 -0300

    .

--- ads-poo/tarde-aula7/src/edu/curso/SistemaSolar.java
+++ ads-poo/tarde-aula7/src/edu/curso/SistemaSolar.java
@@ -12,8 +12,17 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 public class SistemaSolar extends Application {
 	class Ponto { 
-		double angulo = 0;
+		double anguloTerra = 0;
+		double anguloMarte = 0;
 	}
+	
+	public double[] centro(Image img, double centroX, double centroY) { 
+		double[] pos = new double[2];
+		pos[0] = centroX - (img.getWidth() / 2);
+		pos[1] = centroY - (img.getHeight() / 2);
+		return pos;
+	}
+	
 	public void start(Stage stage) { 
 		Group grp = new Group();
 		Scene scn = new Scene(grp);
@@ -33,15 +42,24 @@ public class SistemaSolar extends Application {
 		new AnimationTimer() {
 			@Override
 			public void handle(long now) {
-				double terraX = 0;
-				double terraY = 0;
 				double terraDistancia = 100;
+				double marsDistancia = 150;
+				
 				ctx.drawImage(imgSpace, 0, 0);
-				ctx.drawImage(imgSun, solX, solY);
-				terraX = (solX + imgSun.getWidth() / 2) + (terraDistancia * Math.cos(p.angulo));
-				terraY = (solY + imgSun.getHeight() / 2) + (terraDistancia * Math.sin(p.angulo));
-				ctx.drawImage(imgEarth, terraX, terraY);
-				p.angulo += 0.01;
+				double[] sunPos = centro(imgSun, solX, solY);
+				ctx.drawImage(imgSun, sunPos[0], sunPos[1]);
+
+				double terraX = solX + (terraDistancia * Math.cos(p.anguloTerra));
+				double terraY = solY + (terraDistancia * Math.sin(p.anguloTerra));
+				double[] earthPos = centro(imgEarth, terraX, terraY);
+				ctx.drawImage(imgEarth, earthPos[0], earthPos[1]);
+				
+				double marsX = solX + (marsDistancia * Math.cos(p.anguloMarte));
+				double marsY = solY + (marsDistancia * Math.sin(p.anguloMarte));
+				double[] marsPos = centro(imgMars, marsX, marsY);
+				ctx.drawImage(imgMars, marsPos[0], marsPos[1]);				
+				p.anguloTerra += 0.01;
+				p.anguloMarte += 0.03;
 			} 
 		}.start();
 		stage.setTitle("Sistema Solar");
