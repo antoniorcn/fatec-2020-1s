5836fa1b90a828e41047aa40f043f024bdbbb764
Antonio Carvalho <antoniorcn@hotmail.com>
2020-06-08 08:38:32 -0300

    IDW Manha Aula 8

---
+++ djd-idw/manha-aula8/petController.php
@@ -0,0 +1,17 @@
+<h1>Bem vindo ao Pet Controller</h1>
+
+<?php
+    $nome = $_REQUEST
+    $raca = $_REQUEST['RACA'];
+
+
+    $pet = new StdClass;
+    $pet->NOME = $nome;
+    $pet->RACA = $raca;
+    $pet->PESO = $peso;
+    $pet->NASC = $nasc;
+
+    $lista_pets = array();
+    array_push($lista_pets)
+
+?>
