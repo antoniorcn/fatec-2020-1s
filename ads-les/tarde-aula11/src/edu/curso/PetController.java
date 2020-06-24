800b28e4636b49ca33ad1371d0499af9ac748b9d
Antonio Carvalho <antoniorcn@hotmail.com>
2020-06-24 17:59:26 -0300

    LES Tarde Aula 11 parte 2

--- ads-les/tarde-aula11/src/edu/curso/PetController.java
+++ ads-les/tarde-aula11/src/edu/curso/PetController.java
@@ -1,9 +1,12 @@
 package edu.curso;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -21,10 +24,22 @@ public class PetController {
 	}
 	
 	@PostMapping("/pet")
-	public ModelAndView processaPet(Pet p) {
-		petRepository.save(p);
+	public ModelAndView processaPet(Pet p, @RequestParam("cmd") String cmd) {
+		String msg = null;
 		ModelAndView mv = new ModelAndView("pet");
 		mv.addObject("PET", p);
+		if ("adicionar".equals(cmd)) {
+			petRepository.save(p);
+			mv.addObject("PET", new Pet());
+			msg = String.format("Pet %s foi adicionado com sucesso", p.getNome());
+		} else if ("pesquisar".equals(cmd)) { 
+			Set<Pet> pets = petRepository.searchByNome(p.getNome());
+			msg = String.format("Foram encontrados %d pets", pets.size());
+			if (pets.size() > 0) { 
+				mv.addObject("LISTA", pets);
+			}
+		}
+		mv.addObject("MENSAGEM", msg);
 		return mv;
 	}
 
