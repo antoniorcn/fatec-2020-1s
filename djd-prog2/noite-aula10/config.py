678be41435b34840932a97a17910b05d7041dbab
Antonio Carvalho <antoniorcn@hotmail.com>
2020-06-27 12:11:36 -0300

    PROG2 Aula 10 Noite parte 2

--- djd-prog2/noite-aula10/config.py
+++ djd-prog2/noite-aula10/config.py
@@ -26,3 +26,26 @@ def get_image_bygid(spritesheet, gid, columns=12, w=16, h=16, space_h=0, space_v
     x = (coluna * (w + space_h)) + left
     y = (linha * (h + space_v)) + top
     return spritesheet.subsurface((x, y), (w, h))
+
+
+class ListaCircularImage:
+    def __init__(self, spritesheet, columns=12, gids=[], delay=10):
+        self.img_lista = []
+        for gid in gids:
+            img = get_image_bygid(spritesheet, gid, columns=columns)
+            self.img_lista.append(img)
+        self.index = 0
+        self.delay = delay
+        self.count_delay = 0
+
+    def get_image(self):
+        self.count_delay += 1
+        if self.count_delay >= self.delay:
+            self.index += 1
+            if self.index >= len(self.img_lista):
+                self.index = 0
+            self.count_delay = 0
+        return self.img_lista[self.index]
+
+
+
--- djd-prog2/noite-aula10/heroi.py
+++ djd-prog2/noite-aula10/heroi.py
@@ -2,16 +2,24 @@ import pygame
 from config import *
 
 
+def get_image(spritesheet, gid):
+    img = get_image_bygid(spritesheet, gid, columns=12)
+    return pygame.transform.scale(img, (BLOCK_W, BLOCK_H))
+
+
 class Boy(pygame.sprite.Sprite):
     def __init__(self, spritesheet):
         pygame.sprite.Sprite.__init__(self)
-        boy_orig = get_image_bygid(spritesheet, 31, columns=12)
-        self.image = pygame.transform.scale(boy_orig, (BLOCK_W, BLOCK_H))
+        self.img_lista = [get_image(spritesheet, 27), get_image(spritesheet, 28), get_image(spritesheet, 29)]
+        self.image = self.img_lista[0]
         self.rect = pygame.Rect((100, 100), (BLOCK_W, BLOCK_H))
         self.vel_x = 0
         self.vel_y = 0
         self.gravidade = 0.009
         self.intencao_pos = list(self.rect.center)
+        self.img_index = 0
+        self.img_delay = 50
+        self.img_count_delay = 0
 
     def mover_esquerda(self):
         self.vel_x = -1
@@ -27,6 +35,13 @@ class Boy(pygame.sprite.Sprite):
         self.vel_y += self.gravidade
         self.intencao_pos[0] += self.vel_x
         self.intencao_pos[1] += self.vel_y
+        self.img_count_delay += 1
+        if self.img_count_delay > self.img_delay:
+            self.img_index += 1
+            if self.img_index >= len(self.img_lista):
+                self.img_index = 0
+            self.image = self.img_lista[self.img_index]
+            self.img_count_delay = 0
 
     def parar_horizontal(self):
         self.vel_x = 0
